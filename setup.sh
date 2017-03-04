d9b652556c8218e21ac3051d93dfce271a57467c
Caleb Millar <caleb@ntm.bz>
2017-03-04 22:09:43 +1300

    Ansible setup will now copy saves from local machine

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Check for existing saves files
+if [ -f "saves.tar.gz" ]; then
+    # Will unpack if exists
+    tar -xvzf saves.tar.gz
+fi
+
 # Setup config
 cp docker-compose-example.yml docker-compose.yml
 cp server-settings.example.json server-settings.json
